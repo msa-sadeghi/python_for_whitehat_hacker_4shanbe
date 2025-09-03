171bb298fd73cb0bba33492359d6f28386f29023
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-09-03 16:59:36 +0330

    end

--- session6.py
+++ session6.py
@@ -17,7 +17,30 @@
 # brute_force()
 
 
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, send_file
+from PIL import Image, ImageDraw, ImageFont
+import io
+import random
+import string
+
+def generate_captcha_text(length=5):
+    return "".join(random.choices(string.ascii_uppercase + \
+                          string.digits, k = length))
+
+
+def generate_captcha_image(text):
+    img = Image.new('RGB', (150, 60), color=(255, 255,255))
+    d = ImageDraw.Draw(img)
+    try:
+        font = ImageFont.truetype("arial.ttf", 40)
+    except:
+        font = ImageFont.load_default()
+    d.text((10, 10), text, font=font, fill=(0,0,0))
+    buf = io.BytesIO()
+    img.save(buf, format='PNG')
+    buf.seek(0)
+    return buf
+
 
 app = Flask(__name__)
 app.secret_key = 'rrrrr'
@@ -29,16 +52,34 @@ html_form = '''
 {% endif %}
 Username: <input type="text" name="username"><br>
 Password: <input type="password" name="password"><br>
+{% if show_captcha %}
+<img src="{{url_for('captcha_image')}}" alt="CAPTCHA"><br><br>
+Captcha: <input type="text" name="captcha"><br>
+{% endif %}
+
 <input type="submit" value="Login">
 </form>
 '''
 
+
+@app.route("/captcha.png")
+def captcha_image():
+    global text
+    img = generate_captcha_image(text)
+    return send_file(img, mimetype='image/png')
+
+
+
 correct_username = "user1"
 correct_password = "pass1"
 attempts = 0
+text = ""
+show_captcha = False
 @app.route("/", methods=["GET", "POST"])
 def login():
     global attempts
+    global show_captcha
+    global text
 
     message = ""
     if request.method == "POST":
@@ -48,7 +89,13 @@ def login():
         # فقط یک بار افزایش می‌دهیم
         attempts += 1
         if attempts > 2:
+            show_captcha = True
             message = "CAPTCHA created! لطفا ثابت کنید انسان هستید."
+            text = generate_captcha_text()
+            user_text = request.form.get("captcha", "")
+            if user_text != text:
+                message = "Wrong Captcha"
+
         elif username == correct_username and password == correct_password:
             message = "Login success!"
             attempts = 0
@@ -56,7 +103,8 @@ def login():
             message = "Invalid username or password."
 
     print("attempts", attempts)
-    return render_template_string(html_form, message=message)
+    return render_template_string(html_form, message=message,\
+                                   show_captcha=show_captcha)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
--- test_session6.py
+++ test_session6.py
@@ -2,31 +2,48 @@ import requests
 from tqdm import tqdm
 import time
 import json
-url = "http://127.0.0.1:5000/"
-def test_login(username, password_list):
+from PIL import Image
+import pytesseract
+from io import BytesIO
 
-    results = []
-    for password in tqdm(password_list):
-        data = {
-            "username": username,
-            "password": password
-        }
-        response  = requests.post(url, data=data)
-        print("----------", "CAPTCHA" in response.text)
-        if "CAPTCHA" in response.text:
-            print(f"captcha created ")
-            time.sleep(4)
-            continue
-        if "login success" in response.text:
-            print(f"correct password is :{password}")
-            results.append({"username": username, "password":password, "success":True})
-            break
-        time.sleep(0.1)
-        # else:
-        #     print(f"{password} is incorrect")
-        #     return False
-    with open("results.json", "w") as f:
-        json.dump(results, f, indent=4, ensure_ascii=False)
+
+url = "http://127.0.0.1:5000/"
+
+
+def solve_captcha():
+    # r = requests.get(url + "captcha.png")
+    # img = Image.open(BytesIO(r.content))
+    img = Image.open("./captcha.png")
+    text = pytesseract.image_to_string(img)
+    return text
+
+print(solve_captcha())
+
+
+# def test_login(username, password_list):
+
+#     results = []
+#     for password in tqdm(password_list):
+#         data = {
+#             "username": username,
+#             "password": password
+#         }
+#         response  = requests.post(url, data=data)
+#         print("----------", "CAPTCHA" in response.text)
+#         if "CAPTCHA" in response.text:
+#             print(f"captcha created ")
+#             time.sleep(4)
+#             continue
+#         if "login success" in response.text:
+#             print(f"correct password is :{password}")
+#             results.append({"username": username, "password":password, "success":True})
+#             break
+#         time.sleep(0.1)
+#         # else:
+#         #     print(f"{password} is incorrect")
+#         #     return False
+#     with open("results.json", "w") as f:
+#         json.dump(results, f, indent=4, ensure_ascii=False)
     
-password_list = ["1234", "password", "blalalal","sssss", "pass1"]
-test_login("user1", password_list)
+# password_list = ["1234", "password", "blalalal","sssss", "pass1"]
+# test_login("user1", password_list)
