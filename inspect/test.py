f2e9e001fb0fb21404cf4af974b270b05a2c57c6
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-09-24 16:58:29 +0330

    s

---
+++ inspect/test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import time
+
+
+driver = webdriver.Edge()
+driver.get("file:///E:/NIKLEARNING/projects/monday/python_for_hackers_4shanbe/inspect/index.html")
+
+
+time.sleep(2)
